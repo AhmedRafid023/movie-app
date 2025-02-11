a66cf9bf09464fcbec6f31683397c5f70d13785b
Rafid <2018-025-318@student.cse.du.ac.bd>
2025-02-11 18:38:57 +0600

    feat: make movie card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -86,8 +87,9 @@ const App = () => {
                         <p className="text-red-500">{errorMessage}</p>
                     ) : (
                         <ul>
-                            {movieList.map((movie) => (
-                                <p key={movie.id} className="text-white">{movie.title}</p>
+                            {movieList
+                                .map((movie) => (
+                                <MovieCard key={movie.id} movie={movie}/>
                             ))}
                         </ul>
                     )}
